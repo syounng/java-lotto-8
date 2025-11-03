92694a9daf99299ac681f85421d461449b569cd6
syounng <stellaj0522@gmail.com>
2025-11-03 23:15:42 +0900

    fix(Application): try-catch 처리
    - ApplicationTest의 runException()을 통과시키기 위해 수정
    - runException()은 내부적으로 예외가 발생하면 그 메시지가 System.out으로 출력된다고 가정하는데, Application.main()이 try-catch로 감싸지지 않아서 예외가 위로 전파됨
    - catch로 예외를 잡아 출력하여 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,13 +11,17 @@ import service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        InputValidation inputValidation = new InputValidation();
-        ConsoleInput consoleInput = new ConsoleInput();
-        InputView inputView = new InputView(consoleInput, inputValidation);
-        OutputView outputView = new OutputView();
-        WinningStats winningStats = new WinningStats(outputView);
-        LottoService lottoService = new LottoService(winningStats);
-        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
-        lottoController.run();
+        try {
+            InputValidation inputValidation = new InputValidation();
+            ConsoleInput consoleInput = new ConsoleInput();
+            InputView inputView = new InputView(consoleInput, inputValidation);
+            OutputView outputView = new OutputView();
+            WinningStats winningStats = new WinningStats(outputView);
+            LottoService lottoService = new LottoService(winningStats);
+            LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

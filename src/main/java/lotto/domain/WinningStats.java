6e1d503de153294d2f337e91798b2edb28f46329
syounng <stellaj0522@gmail.com>
2025-11-03 19:42:14 +0900

    refactor(OutputView): 당첨 통계 출력 로직을 View로 이동
    - 각 객체의 책임을 분명히 하기 위해 출력 로직을 view로 분리했습니다.

--- src/main/java/lotto/domain/WinningStats.java
+++ src/main/java/lotto/domain/WinningStats.java
@@ -4,20 +4,7 @@ import java.util.Map;
 
 public class WinningStats {
 
-    private final WinningNumber winningNumber;
-
-    public WinningStats(WinningNumber winningNumber) {
-        this.winningNumber = winningNumber;
-    }
-
     public void printStats(Lottos lottos){
-        Map<Rank, Integer> results = lottos.calcRank(winningNumber);
-        System.out.println("당첨 통계");
-        System.out.println("---------");
-        System.out.printf("3개 일치 (5,000원) - %d개%n", results.getOrDefault(Rank.FIFTH, 0));
-        System.out.printf("4개 일치 (50,000원) - %d개%n", results.getOrDefault(Rank.FOURTH, 0));
-        System.out.printf("5개 일치 (1,500,000원) - %d개%n", results.getOrDefault(Rank.THIRD, 0));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", results.getOrDefault(Rank.SECOND, 0));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", results.getOrDefault(Rank.FIRST, 0));
+        Map<Rank, Integer> results = lottos.calcRank(WinningNumber.INSTANCE);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.util.Map;
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
 
 public class OutputView {
 
@@ -9,7 +11,13 @@ public class OutputView {
         lottos.printLottos();
     }
 
-    public void printWinningStats(){
-
+    public void printWinningStats(Map<Rank, Integer> results){
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.printf("3개 일치 (5,000원) - %d개%n", results.getOrDefault(Rank.FIFTH, 0));
+        System.out.printf("4개 일치 (50,000원) - %d개%n", results.getOrDefault(Rank.FOURTH, 0));
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n", results.getOrDefault(Rank.THIRD, 0));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", results.getOrDefault(Rank.SECOND, 0));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", results.getOrDefault(Rank.FIRST, 0));
     }
 }

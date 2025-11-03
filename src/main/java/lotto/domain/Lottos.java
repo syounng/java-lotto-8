194d6db2fd5865a0a6a002370c356be758ba6ef6
syounng <stellaj0522@gmail.com>
2025-11-03 16:45:59 +0900

    refactor(LottoService): 로또 리스트 생성 로직을 서비스->도메인으로 옮김
    - Lottos가 자기 자신을 생성
    - 팩토리 메서드 패턴을 사용
    - 생성자를 private으로 변경하여 유효한 방식으로만 생성되게 함

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,11 +1,20 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
     private final List<Lotto> lottos;
 
-    public Lottos(List<Lotto> lottos){
+    private Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public static Lottos fromCount(int count) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottoList.add(Lotto.createRandomLotto());
+        }
+        return new Lottos(lottoList);
     }
 }
--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,36 +1,19 @@
 package service;
 
-import lotto.domain.Lotto;
 import lotto.domain.Lottos;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LottoService {
 
     private static final int LOTTO_PRICE = 1000;
 
     public Lottos generateLottos(int purchasePrice) {
-        int numberOfLotto = calcNumberOfLotto(purchasePrice);
-        return issueLottos(numberOfLotto);
+        validatePrice(purchasePrice);
+        int count = purchasePrice / LOTTO_PRICE;
+        return Lottos.fromCount(count);
     }
 
-    private int calcNumberOfLotto(int price) {
-        isValidatePrice(price);
-        return price / LOTTO_PRICE;
-    }
-
-    private Lottos issueLottos(int count) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            lottoList.add(Lotto.createRandomLotto());
-        }
-        return new Lottos(lottoList);
-    }
-
-    private void isValidatePrice(int price){
+    private void validatePrice(int price) {
         if (price < LOTTO_PRICE || price % LOTTO_PRICE != 0)
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
     }
-
 }

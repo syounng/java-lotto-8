41138890ffef8a139b2137e34c73a23809ff68d0
syounng <stellaj0522@gmail.com>
2025-11-03 19:46:00 +0900

    feat(Application): 애플리케이션 로직 구현
    - 애플리케이션 진입점
    - Controller.run()으로 앱을 구동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.WinningNumber;
+import lotto.domain.WinningStats;
+import lotto.view.ConsoleInput;
+import lotto.view.InputValidation;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputValidation inputValidation = new InputValidation();
+        ConsoleInput consoleInput = new ConsoleInput();
+        InputView inputView = new InputView(consoleInput, inputValidation);
+        OutputView outputView = new OutputView();
+        WinningStats winningStats = new WinningStats();
+        LottoService lottoService = new LottoService(winningStats);
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+        lottoController.run();
     }
 }

ec29eb806ef9375502001c8f3712a3a42df83817
syounng <stellaj0522@gmail.com>
2025-11-03 19:29:09 +0900

    feat(LottoService): 당첨 번호 생성 로직 구현
    String을 파싱하여 싱글톤 enum값을 생성합니다.

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,10 +1,18 @@
 package service;
 
+import java.util.*;
 import lotto.domain.Lottos;
+import lotto.domain.WinningNumber;
 
 public class LottoService {
 
     private static final int LOTTO_PRICE = 1000;
+
+    private WinningNumber winningNumber;
+
+    public LottoService(WinningNumber winningNumber){
+        this.winningNumber = winningNumber;
+    }
 
     public Lottos generateLottos(int purchasePrice) {
         validatePrice(purchasePrice);
@@ -12,6 +20,16 @@ public class LottoService {
         return Lottos.fromCount(count);
     }
 
+    public void generateWinningNumber(String winningNumbersInput, String bonusNumberInput){
+        String[] numbers = winningNumbersInput.split(",");
+        int bonusNumber = Integer.parseInt(bonusNumberInput);
+        List<Integer> winningNumbers = Arrays.stream(numbers)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        WinningNumber.INSTANCE.initialize(winningNumbers, bonusNumber);
+    }
+    
     private void validatePrice(int price) {
         if (price < LOTTO_PRICE || price % LOTTO_PRICE != 0)
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
